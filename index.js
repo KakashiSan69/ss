79b03062a6165cba47e4f87e8e55154edb50e1d7
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2025-03-23 23:19:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import puppeteer from "puppeteer";
+const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
 const PORT = 8000;
